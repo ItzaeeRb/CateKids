3699e4e4548b3b12ec402e5a2e150679db1acd71
Itzae Rivera <itzaee19@gmail.com>
2025-02-09 17:23:45 -0600

    Conexión a la base de datos

---
+++ Conexion/Conexion.php
@@ -0,0 +1,10 @@
+<?php
+	$con = new mysqli('localhost', 'admincatekids', '$123Redprueb4$', 'CateKids');
+	$con->set_charset("utf8mb4");
+	if(!$con){
+		die("ERROR: Couldn't connect to database");
+    }else{
+        echo "Conexión exitosa!";
+    }
+?>
+
