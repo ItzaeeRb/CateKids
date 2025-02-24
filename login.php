141235f4aece45ca05fc4ca9d1d9871259fc7c0c
Itzae Rivera <itzaee19@gmail.com>
2025-02-23 23:04:30 -0600

    pagina de inicio

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="Templates/css/style_login.css"/>
+
+</head>
+<body>
+
+<?php
+include 'Templates/login.html';
+
+?>
+    
+</body>
+</html>
