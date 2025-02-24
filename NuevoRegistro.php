bdf115d4d1d2303e30f00aca55b54c79878b8467
Itzae Rivera <itzaee19@gmail.com>
2025-02-23 23:22:11 -0600

    Diseño formulario de registro para catequistas

--- NuevoRegistro.php
+++ NuevoRegistro.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-    <link rel="stylesheet" href="Templates/css/style_login.css"/>
+    <title>Inscripción</title>
+    <link rel="stylesheet" href="Templates/css/style_index.css"/>
 
 </head>
 <body>
 
 <?php
-include 'Templates/login.html';
+include 'Templates/index.html';
 
 ?>
     
